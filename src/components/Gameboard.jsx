b7dd3daa32d37495f35c5dcd8363a9edf1dcce46
sudhanshu kumar <sudhanshu_ext@datanimbus.com>
2024-01-15 12:20:44 +0530

    added the gameboard component & its feature

---
+++ src/components/Gameboard.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const initialGameboard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+    const [gameBoard, setGameBoard] = useState(initialGameboard);
+
+    const handleSelectSquare = (rowIndex, colIndex) => {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        })
+        onSelectSquare();
+    }
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))
+
+            }
+        </ol>
+    )
+}
