74222252afa0c3adf977cbba9c1e447bbd644ff5
sudhanshu kumar <sudhanshu_ext@datanimbus.com>
2024-01-15 12:18:48 +0530

    added player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+    const [newName, setNewName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+
+    const handleName = () => {
+        setIsEditing(editing => !editing);
+    }
+    const handleChange = (event) => {
+        setNewName(event.target.value)
+    }
+    return (
+        <li className={isActive ? 'active' : undefined}>
+            <span className="player">
+                {!isEditing ? (<span className="player-name">{newName}</span>) : <input type="text" required value={newName} onChange={handleChange} />}
+
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleName}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
+}
